75d33e51a75891a9b3ccbe86b5d9930db7608dd8
JC BUISSON <buisson@enseeiht.fr>
2023-02-21 13:21:41 +0100

    WIP

---
+++ config/default.js
@@ -0,0 +1,37 @@
+
+module.exports = {
+
+   authentication: {
+      entity: "user",
+      service: "user",
+      secret: "A3lxcjmVD0af6Hjd1fksZyzFGtE=",
+      authStrategies: [
+         "jwt",
+         "local"
+      ],
+      jwtOptions: {
+         header: {
+            typ: "access"
+         },
+         audience: "https://mitab.com",
+         issuer: "feathers",
+         algorithm: "HS256",
+         expiresIn: "100y"
+      },
+      local: {
+         usernameField: "email",
+         passwordField: "password"
+      }
+   },
+
+   NODEMAILER: {
+      host: "smtp.online.net",
+      port: 587,
+      secure: false,
+      auth: {
+         user: "contact@shdl.fr",
+         pass: "Eurek@31",
+      },
+      name: "shdl.fr",
+   },
+}
