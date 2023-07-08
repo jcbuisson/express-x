b0bca513c571baf2fbf64c814e68bf1084e043d6
JC BUISSON <buisson@enseeiht.fr>
2023-07-08 06:42:43 +0200

    Removed Connection as service

--- src/context.mjs
+++ src/context.mjs
@@ -1,13 +1,13 @@
 
 export async function getContextConnection(context) {
    const id = context.params.connectionId
-   const connection = await context.app.service('Connection')._findUnique({ where: { id }})
+   const connection = await context.app.prisma.Connection.findUnique({ where: { id }})
    return connection
 }
 
 export async function resetConnection(context) {
    const id = context.params.connectionId
-   await context.app.service('Connection')._update({
+   await context.app.prisma.Connection.update({
       where: { id },
       data: {
          clientIP: '',
@@ -19,17 +19,17 @@ export async function resetConnection(context) {
 
 export async function getConnectionDataItem(context, key) {
    const id = context.params.connectionId
-   const connection = await context.app.service('Connection')._findUnique({ where: { id }})
+   const connection = await context.app.prisma.Connection.findUnique({ where: { id }})
    const data = JSON.parse(connection.data)
    return data[key]
 }
 
 export async function setConnectionDataItem(context, key, value) {
    const id = context.params.connectionId
-   const connection = await context.app.service('Connection')._findUnique({ where: { id }})
+   const connection = await context.app.prisma.Connection.findUnique({ where: { id }})
    const data = JSON.parse(connection.data)
    data[key] = value
-   await context.app.service('Connection')._update({
+   await context.app.prisma.Connection.update({
       where: { id },
       data: {
          data: JSON.stringify(data)
@@ -39,10 +39,10 @@ export async function setConnectionDataItem(context, key, value) {
 
 export async function removeConnectionDataItem(context, key) {
    const id = context.params.connectionId
-   const connection = await context.app.service('Connection')._findUnique({ where: { id }})
+   const connection = await context.app.prisma.Connection.findUnique({ where: { id }})
    const data = JSON.parse(connection.data)
    delete data[key]
-   await context.app.service('Connection').update({
+   await context.app.prisma.Connection.update({
       where: { id },
       data: {
          data: JSON.stringify(data)
--- src/server.mjs
+++ src/server.mjs
@@ -399,7 +399,7 @@ export function expressX(prisma, options = {}) {
    }
 
    async function getChannelConnections(channelName) {
-      const connections = await app.service('Connection').findMany({})
+      const connections = await app.prisma.Connection.findMany({})
       return connections.filter(connection => {
          const channelNames = JSON.parse(connection.channelNames)
          return channelNames.includes(channelName)
@@ -409,7 +409,7 @@ export function expressX(prisma, options = {}) {
    async function addChannelToConnection(connection, channelName) {
       const channelNames = JSON.parse(connection.channelNames)
       if (!channelNames.includes(channelName)) channelNames.push(channelName)
-      await app.service('Connection').update({
+      await app.prisma.Connection.update({
          where: { id: connection.id },
          data: { channelNames: JSON.stringify(channelNames) },
       })
@@ -417,7 +417,7 @@ export function expressX(prisma, options = {}) {
    
    async function removeChannelFromConnection(connection, channelName) {
       const channelNames = JSON.parse(connection.channelNames).filter(name => name !== channelName)
-      await app.service('Connection').update({
+      await app.prisma.Connection.update({
          where: { id },
          data: { channelNames: JSON.stringify(channelNames) },
       })
