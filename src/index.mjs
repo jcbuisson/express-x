3b00927a2bbe155e9771adb38856582fa26d0681
JC BUISSON <buisson@enseeiht.fr>
2025-03-01 07:19:03 +0100

    Removed auth hooks

--- src/index.mjs
+++ src/index.mjs
@@ -305,14 +305,13 @@ export class EXError extends Error {
 }
 
 
-
+//////////////////////////       HOOK METHODS       //////////////////////////
 
 /*
  * Add a timestamp property of name `field` with current time as value
 */
 export const addTimestamp = (field) => async (context) => {
    context.result[field] = (new Date()).toISOString()
-   return context
 }
 
 /*
@@ -321,84 +320,19 @@ export const addTimestamp = (field) => async (context) => {
 export const hashPassword = (passwordField) => async (context) => {
    const user = context.result
    user[passwordField] = await bcrypt.hash(user[passwordField], 5)
-   return context
 }
 
 /*
  * Remove `field` from `context.result`
 */
-export function protect(field) {
-   return async (context) => {
-      if (context.result) {
-         if (Array.isArray(context.result)) {
-            for (const value of context.result) {
-               delete value[field]
-            }
-         } else if (typeof context.result === "object") {
-            delete context.result[field]
+export const protect = (field) => (context) => {
+   if (context.result) {
+      if (Array.isArray(context.result)) {
+         for (const value of context.result) {
+            delete value[field]
          }
+      } else if (typeof context.result === "object") {
+         delete context.result[field]
       }
-      return context
    }
-}
-
-export const isNotExpired = async (context) => {
-   // do nothing if it's not a client call from a ws connexion
-   if (!context.socket) return
-   const expiresAt = context.socket?.data?.expiresAt
-   if (expiresAt) {
-      const expiresAtDate = new Date(expiresAt)
-      const now = new Date()
-      if (now > expiresAtDate) {
-         // expiration date is met
-         // clear socket.data
-         context.socket.data = {}
-         // leave all rooms except socket#id
-         const rooms = new Set(context.socket.rooms)
-         for (const room of rooms) {
-            if (room === context.socket.id) continue
-            context.socket.leave(room)
-         }
-         // send an event to the client (typical client handling: logout)
-         context.socket.emit('not-authenticated')
-         // throw exception
-         throw new EXError('not-authenticated', "Session expired")
-      }
-   } else {
-      // send an event to the client (typical client handling: logout)
-      context.socket.emit('not-authenticated')
-      throw new EXError('not-authenticated', "No expiresAt in socket.data")
-   }
-}
-
-/*
- * Throw an error for a client service method call when socket.data does not contain user
-*/
-export const isAuthenticated = async (context) => {
-   // do nothing if it's not a client call from a ws connexion
-   if (context.caller !== 'client') return
-   if (!context.socket?.data) {
-      // send an event to the client (typical client handling: logout)
-      context.socket.emit('not-authenticated')
-      throw new EXError('not-authenticated', 'no data in socket')
-   }
-   if (!context.socket.data?.user) {
-      // send an event to the client (typical client handling: logout)
-      context.socket.emit('not-authenticated')
-      throw new EXError('not-authenticated', 'no user in socket.data')
-   }
-}
-
-/*
- * Extend value of socket.data.expiresAt of `duration` milliseconds
-*/
-export const extendExpiration = (duration) => async (context) => {
-   const now = new Date()
-   if (context.caller !== 'client') return
-   if (!context.socket?.data) {
-      // send an event to the client (typical client handling: logout)
-      context.socket.emit('not-authenticated')
-      throw new EXError('not-authenticated', 'no data in socket')
-   }
-   context.socket.data.expiresAt = new Date(now.getTime() + duration)
 }
